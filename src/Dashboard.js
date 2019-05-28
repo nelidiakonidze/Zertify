61674f7a758136bb48fa8d3892802a93946c0a56
Neli <diakonidzeneli@gmail.com>
2019-05-28 11:04:52 +0200

    material button version 1 done

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import ButtonCreate from './ButtonCreate';
+import MyButton from './ButtonCreate';
 
 function Dashboard() {
   return (
     <div className='App'>
       <h1>Helllllo zertify!</h1>
+      <MyButton />
     </div>
   );
 }
