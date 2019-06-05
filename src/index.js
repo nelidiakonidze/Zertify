dfaf40eac5a4f4a3edddec4aa793cae24a6f2c81
hiijs <bernadetta.nycz@icloud.com>
2019-06-05 17:27:09 +0200

    preview cards

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Dashboard from './Dashboard';
+import ZTemplate from './components/ZTemplate/ZTemplate';
 
-ReactDOM.render(<Dashboard />, document.getElementById('root'));
+ReactDOM.render(<ZTemplate />, document.getElementById('root'));
