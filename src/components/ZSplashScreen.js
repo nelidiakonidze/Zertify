429d2cfa3686ab5077f1a409d982b4a0056b9a1a
Raphael Lautenbacher <datingel@Raphaels-MacBook-Pro.local>
2019-06-21 11:56:23 +0200

    added SplashScreen components for Home page

---
+++ src/components/ZSplashScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ZSplashScreen = () => {
+  return (
+    <div>
+      <h1>Welcome to Zertify</h1>
+      <h2>Certification Generator</h2>
+      <img src='https://i.imgur.com/K27lINZ.png' alt='TemplatesChoice' />
+      {/* <img src='https://i.imgur.com/lFxUsdV.jpg' alt='TemplatesChoice2' />
+  <img src='https://i.imgur.com/bPBtyoZ.jpg' alt='TemplatesChoice3' /> */}
+    </div>
+  );
+};
+
+export default ZSplashScreen;
