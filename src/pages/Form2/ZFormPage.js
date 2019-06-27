c95c002f1b7cbddbaf00dced43c2677ea5ef4eaf
Campalo <25295489+Campalo@users.noreply.github.com>
2019-06-27 17:11:34 +0200

    still debugging

--- src/pages/Form2/ZFormPage.js
+++ src/pages/Form2/ZFormPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ZSideNav from '../../components/layout/ZSideNav';
-import ZCourseForm from '../../components/form2/ZCourseForm';
-import ZStudentForm from '../../components/form2/ZStudentForm';
+// import ZStudentForm from '../../components/form2/ZStudentForm';
+import ZCourseForm from '../../components/forms2/ZCourseForm';
 import ZFooter from '../../components/layout/ZFooter';
 import './ZFormsPage.css';
 
@@ -13,7 +13,7 @@ const ZFormsPage = () => {
         <h2>Add a new course to the course list</h2>
         <ZCourseForm />
         <h2>Add a new student to the student list</h2>
-        <ZStudentForm />
+        {/* <ZStudentForm /> */}
         <ZFooter />
       </section>
     </div>
