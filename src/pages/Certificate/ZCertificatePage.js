6de067b99e34e4f988552cb0de12557c7245079b
Campalo <25295489+Campalo@users.noreply.github.com>
2019-07-05 12:18:23 +0200

    create the certificate in pdf (same design for now)

--- src/pages/Certificate/ZCertificatePage.js
+++ src/pages/Certificate/ZCertificatePage.js
@@ -1,25 +1,23 @@
 import React from 'react';
-//import ZCertificate from '../../components/ZCertificate';
+import ZCertificate from '../../components/certificates/ZCertificate';
 import {PDFViewer} from '@react-pdf/renderer';
 import ZpdfCertificate from '../../components/certificates/ZpdfCertificate';
 import './ZCertificatePage.css';
 
 const ZCertificatePage = () => {
   return (
-    <React.Fragment>
-      {/* <section className='flex-certificate'>
-        <ZCertificate />
-      </section> */}
-      <section className='flex-btn'>
+    <div className='width-div'>
+      {/* <ZCertificate /> */}
+      {/* <section className='flex-btn'>
         <button className='button select-table-btn'>
           Send Certificate to student
         </button>
         <button className='button select-back-btn'>Go back</button>
-      </section>
-      {/* <PDFViewer>
+      </section> */}
+      <PDFViewer>
         <ZpdfCertificate />
-      </PDFViewer> */}
-    </React.Fragment>
+      </PDFViewer>
+    </div>
   );
 };
 
