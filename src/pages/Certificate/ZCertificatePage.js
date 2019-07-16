a8644350ee8b7ce3491a17146c4fe5321ee31427
hiijs <bernadetta.nycz@icloud.com>
2019-07-16 16:27:04 +0200

    added motifited files

--- src/pages/Certificate/ZCertificatePage.js
+++ src/pages/Certificate/ZCertificatePage.js
@@ -8,15 +8,14 @@ const ZCertificatePage = ({selectedStudent, selectedColor, sendEmail}) => {
   return (
     <div>
       <section className='flex-btn'>
-<<<<<<< HEAD
          <Link to= '/certificate/sent'>
-        <button className='button select-table-btn'>
-=======
+       
+       
         <button className='button select-table-btn' onClick={sendEmail}>
->>>>>>> 2c15f47107f86c3285c5940b99c683a087a972ce
           Send Certificate to student
         </button>
-        </Link>
+
+         </Link>
         <Link to='/'>
           <button className='button select-back-btn'>
             Go back
