6ee9bc46340dac3bf83bbcdfd878e951b184bf6f
Campalo <25295489+Campalo@users.noreply.github.com>
2019-07-23 18:07:53 +0200

    test to position buttons on home page without touching css

--- src/components/ZSplashScreen/ZSplashScreen.js
+++ src/components/ZSplashScreen/ZSplashScreen.js
@@ -6,6 +6,10 @@ const ZSplashScreen = () => {
     <div className='flex-splashscreen'>
       <h1>Welcome to Zertify</h1>
       <h2>The Edera Certification Generator</h2>
+      <section>
+          <button className="button">1</button>
+          <button className="button">2</button>
+      </section>
       <img src='https://i.imgur.com/K27lINZ.png' alt='TemplatesChoice' />
       {/* <img src='https://i.imgur.com/lFxUsdV.jpg' alt='TemplatesChoice2' />
   <img src='https://i.imgur.com/bPBtyoZ.jpg' alt='TemplatesChoice3' /> */}
