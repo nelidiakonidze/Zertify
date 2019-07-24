365f8b6d293c61ea30e800b9469b9f4e84215779
Campalo <25295489+Campalo@users.noreply.github.com>
2019-07-24 11:40:24 +0200

    create the 404 page with title / GIF / navbar

--- src/pages/NoPage/ZNoPage.js
+++ src/pages/NoPage/ZNoPage.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './ZNoPage.css';
+import ZSideNav from '../../components/layout/ZSideNav';
+import ZFooter from '../../components/layout/ZFooter';
 
 const ZNoPage = () => {
   return (
     <div>
-      <p>hi there something went wrong</p>
+      <ZSideNav />
+      <section className='flex-404'>
+        <h1>Something went wrong...</h1>
+        <img src='https://media.giphy.com/media/Bp3dFfoqpCKFyXuSzP/source.gif' />
+        <h2>Please, use the menu to go to another page</h2>
+      </section>
     </div>
   );
 };
