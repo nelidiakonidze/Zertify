03989f28437ca4ad25cac0ee8cd5f6b15717fc35
Campalo <25295489+Campalo@users.noreply.github.com>
2019-07-24 10:48:18 +0200

    fix css to push footer to bottom + reduce img size

--- src/components/ZSplashScreen/ZSplashScreen.js
+++ src/components/ZSplashScreen/ZSplashScreen.js
@@ -4,26 +4,21 @@ import './ZSplashScreen.css';
 
 const ZSplashScreen = () => {
   return (
-    <div>
-      <div>
-        <h1 className='flex-splashscreen'>Welcome to Zertify</h1>
-        <h2 className='flex-splashscreen'>Certification Creator</h2>
-        <div className='instruction_btn'>
-          <NavLink to='/students'>
-            <button className='splashScreenBtn'>Select elegible Student</button>
-          </NavLink>
-          <NavLink to='/form'>
-            <button className='splashScreenBtn'>Add new Student/Course</button>
-          </NavLink>
-        </div>
-        <img
-          className='flex-splashscreen'
-          src='https://i.imgur.com/K27lINZ.png'
-          alt='TemplatesChoice'
-        />
-        {/* <img src='https://i.imgur.com/lFxUsdV.jpg' alt='TemplatesChoice2' />
-    <img src='https://i.imgur.com/bPBtyoZ.jpg' alt='TemplatesChoice3' /> */}
-      </div>
+    <div className='flex-splashscreen'>
+      <h1>Welcome to Zertify</h1>
+      <h2>The Edera Certification Generator</h2>
+      <section>
+        <NavLink to='/students'>
+          <button className='button'>Select an elegible student</button>
+        </NavLink>
+        <NavLink to='/form'>
+          <button className='button'>Create new student or course</button>
+        </NavLink>
+      </section>
+      <img
+        src='https://i.imgur.com/K27lINZ.png'
+        alt='splashscreen illustration from Undraw '
+      />
     </div>
   );
 };
